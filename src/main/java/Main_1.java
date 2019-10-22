ea3a7d6f23d674433b825d4b07635ee9fbafd620
Juliano Montini <julianomontini@gmail.com>
2019-10-22 14:52:17 -0300

    http

--- src/main/java/Main_1.java
+++ src/main/java/Main_1.java
@@ -1,13 +1,21 @@
-import model.Funcionario;
+import com.google.gson.*;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
-/**
- * Básico Classe
- */
 public class Main_1 {
-    public static void main(String... args){
-        Funcionario func = new Funcionario("Juliano", "Analista de sistemas", 24, 4);
-        System.out.println(func.hello());
-        System.out.println(func.getCargo());
-        System.out.println(func);
+    private static String URL = "http://qaleraning.i7m76hdjy3.us-east-1.elasticbeanstalk.com";
+    public static void main(String... args) throws Exception{
+        OkHttpClient client = new OkHttpClient();
+        Request req = new Request.Builder()
+                .url(URL + "/profiles")
+                .get()
+                .build();
+        Response response = client.newCall(req).execute();
+        String body = response.body().string();
+        JsonArray profiles = JsonParser.parseString(body).getAsJsonArray();
+        JsonObject profile = profiles.get(0).getAsJsonObject();
+        System.out.println(profile);
+        System.out.println(profile.get("job").getAsString());
     }
 }
